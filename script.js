2862b3d7b81ce31b7732ebcf95fea280846a1249
Dishank7 <127612933+Dishank7@users.noreply.github.com>
2023-06-21 16:18:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+
+let div = document.getElementById("output");
+
+let prom = new Promise((resolve) => {
+    resolve();
+})
+
+setTimeout(() => {
+    prom.then(()=>{
+     div.innerText = 'Hello World';
+    })
+}, 1000);
